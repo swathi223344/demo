47b57871ef4587f9333ab318c7a0559d8be1cd38
anaga1990 <anaga.java@gmail.com>
2022-01-30 17:57:20 +0530

    maven quick project created

---
+++ E2EP/src/main/java/com/qa/E2EP/App.java
@@ -0,0 +1,13 @@
+package com.qa.E2EP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
